8641f2dded120b070365185eab7197d51edec8bf
michielfbr <michiel_brongers@hotmail.com>
2021-12-01 14:51:09 +0100

    dependency installed | form basics setup

--- bored/src/components/ActivityForm/index.js
+++ bored/src/components/ActivityForm/index.js
@@ -1,5 +1,27 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
 
 export default function ActivityForm() {
-  return <h1>ActivityForm</h1>;
+  return (<>
+  <h1>ActivityForm</h1>
+  <Form>
+    I want to
+     <Form.Group>
+      <Form.Select defaultValue="Choose...">
+        <option disabled selected>select</option>
+        <option>learn something</option>
+        <option>do fun things</option>
+        <option>learn something</option>
+      </Form.Select>
+    </Form.Group>
+    with
+    <Form.Group>
+      <Form.Select defaultValue="Choose...">
+        <option disabled selected>select</option>
+        <option>myself</option>
+        <option>somebody else</option>
+        <option>a lot of people</option>
+      </Form.Select>
+    </Form.Group>
+  </Form></>)
 }
