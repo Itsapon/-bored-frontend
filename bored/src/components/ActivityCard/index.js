92b32c0cd38fdeaf71ce1b9a5be87fddaabbdc8a
michielfbr <michiel_brongers@hotmail.com>
2021-12-01 14:27:57 +0100

    component.ActivityCard & component.ActivityForm setup

---
+++ bored/src/components/ActivityCard/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ActivityCard() {
+  return <h1>ActivityCard</h1>;
+}
---
+++ bored/src/components/ActivityForm/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ActivityForm() {
+  return <h1>ActivityForm</h1>;
+}
