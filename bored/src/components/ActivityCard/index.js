024cdf8fef3970b3d46d7565af54d030e7b0eefc
michielfbr <michiel_brongers@hotmail.com>
2021-12-01 15:38:03 +0100

    Form setup displayed on component.ActivityForm

--- bored/src/components/ActivityCard/index.js
+++ bored/src/components/ActivityCard/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export default function ActivityCard() {
-  return <h1>ActivityCard</h1>;
+  return (
+    <Container>
+      <h1>component.ActivityCard</h1>
+    </Container>
+  );
 }
