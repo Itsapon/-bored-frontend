38736cf94b960ba03ee719f5c16ba885f76be2f8
michielfbr <michiel_brongers@hotmail.com>
2021-12-01 15:16:05 +0100

    Components displayed in App.js for test only

--- bored/src/App.js
+++ bored/src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ActivityCard from "./components/ActivityCard";
+import ActivityForm from "./components/ActivityForm";
+
+const Test = () => (
+  <div>
+    <h1>Test</h1>
+    <ActivityCard />
+    <ActivityForm />
+  </div>
+);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
 }
 
 export default App;
