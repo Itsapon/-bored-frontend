6f810bbf264eff2fcb415a6078dcd6516e0bf6ea
michielfbr <michiel_brongers@hotmail.com>
2021-12-02 12:36:33 +0100

    App styled with random background color from array, fonts & layout.

--- bored/src/App.js
+++ bored/src/App.js
@@ -8,28 +8,15 @@ import {
 
 import "./App.css";
 
-import ActivityCard from "./components/ActivityCard";
-import ActivityForm from "./components/ActivityForm";
-import { Container } from "react-bootstrap";
 import Navigation from "./components/Navigation";
 import Homepage from "./pages/Homepage/Homepage";
 import Activitypage from "./pages/Activitypage";
 
-const Test = () => (
-  <Container className="p-5 mb-4 bg-light rounded-3">
-    <h1 className="header">Testing components</h1>
-    <ActivityCard />
-    <ActivityForm />
-  </Container>
-);
-
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="App" style={{ height: "100vh" }}>
       <BrowserRouter>
         <Navigation />
-        {/* <Test /> */}
-
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/activity" element={<Activitypage />} />
--- bored/src/pages/Activitypage/index.js
+++ bored/src/pages/Activitypage/index.js
@@ -1,13 +1,46 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import ActivityCard from "../../components/ActivityCard";
 import ActivityForm from "../../components/ActivityForm";
+import { useEffect } from "react";
+
+const activitypageStyle = {
+  width: "90%",
+  fontFamily: "Grandstander",
+  fontSize: "25px",
+};
+
+const questionStyle = {
+  fontSize: "50px",
+};
+
+function getNewRandomBackgroundColor() {
+  var myArray = ["#B8B8D1", "#5B5F97", "#FFC145", "#FF6B6C"]; // 'B8B8D1'
+  var rand = myArray[Math.floor(Math.random() * myArray.length)];
+  document.getElementById("App").style.backgroundColor = rand;
+}
+
+function buttonClick() {
+  console.log("Hi, I'm clicked");
+  getNewRandomBackgroundColor();
+}
 
 export default function Activitypage() {
+  useEffect(() => {
+    getNewRandomBackgroundColor();
+  }, []);
+
   return (
-    <div>
-      <h1>Wanna do this?</h1>
-      <ActivityCard />
-      <ActivityForm />
-    </div>
+    <Container id="ActivityPage" style={activitypageStyle}>
+      <Row>
+        <h1 style={questionStyle}>Wanna do this?</h1>
+      </Row>
+      <Row>
+        <ActivityCard />
+      </Row>
+      <Row>
+        <ActivityForm buttonClick={buttonClick} />
+      </Row>
+    </Container>
   );
 }
