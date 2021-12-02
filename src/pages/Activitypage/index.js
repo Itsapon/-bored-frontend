f699ba49e83aea591302a4681893019b8b9e2b8d
Itsapon <robintmier@gmail.com>
2021-12-02 17:09:09 +0100

    moved the dispatch action out of teh useEffect in activitypage to the homepage instead to speed up the displaying

--- src/pages/Activitypage/index.js
+++ src/pages/Activitypage/index.js
@@ -12,39 +12,38 @@ import { selectActivity } from "../../store/activities/selectors";
 export default function Activitypage() {
   const dispatch = useDispatch();
   const newActivity = useSelector(selectActivity);
-  const [activity, setActivity] = useState({});  
+  const [activity, setActivity] = useState({});
   const activitypageStyle = {
-  width: "90%",
-  fontFamily: "Grandstander",
-  fontSize: "25px",
-};
+    width: "90%",
+    fontFamily: "Grandstander",
+    fontSize: "25px",
+  };
 
-const questionStyle = {
-  fontSize: "50px",
-};
+  const questionStyle = {
+    fontSize: "50px",
+  };
 
-function getNewRandomBackgroundColor() {
-  var colorArray = [
-    { backgroundcolor: "#5B5F97", fontcolor: "#FFFFFB" }, // good combo
-    { backgroundcolor: "#FFC145", fontcolor: "#373D20" }, // good combo, also with logo
-    { backgroundcolor: "#FF6B6C", fontcolor: "#FFFFFB" },
-  ];
-  var colorSheme = colorArray[Math.floor(Math.random() * colorArray.length)];
-  document.getElementById("App").style.backgroundColor = colorSheme.backgroundcolor;
-  document.getElementById("App").style.color = colorSheme.fontcolor;
-}
+  function getNewRandomBackgroundColor() {
+    var colorArray = [
+      { backgroundcolor: "#5B5F97", fontcolor: "#FFFFFB" }, // good combo
+      { backgroundcolor: "#FFC145", fontcolor: "#373D20" }, // good combo, also with logo
+      { backgroundcolor: "#FF6B6C", fontcolor: "#FFFFFB" },
+    ];
+    var colorSheme = colorArray[Math.floor(Math.random() * colorArray.length)];
+    document.getElementById("App").style.backgroundColor =
+      colorSheme.backgroundcolor;
+    document.getElementById("App").style.color = colorSheme.fontcolor;
+  }
 
-function buttonClick() {
-  console.log("Hi, I'm clicked");
-  getNewRandomBackgroundColor();
-}
-  
+  function buttonClick() {
+    console.log("Hi, I'm clicked");
+    getNewRandomBackgroundColor();
+  }
+
   useEffect(() => {
     getNewRandomBackgroundColor();
-    console.log("Hi there this is useEffect, imma dispatch fetchrandom");
-    dispatch(fetchRandom());
     setActivity(newActivity);
-  }, [activity]);
+  }, []);
 
   console.log("activity is ", activity);
   return (
@@ -53,7 +52,7 @@ function buttonClick() {
         <h1 style={questionStyle}>Wanna do this?</h1>
       </Row>
       <Row>
-        <ActivityCard activity={activity}/>
+        <ActivityCard activity={activity} />
       </Row>
       <Row>
         <ActivityForm buttonClick={buttonClick} />
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,8 +1,20 @@
+//tool imports
 import Button from "@restart/ui/esm/Button";
-import React from "react";
+import React, { useDebugValue } from "react";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+//actions etc
+import { fetchRandom } from "../../store/activities/actions";
 
+//function
 export default function Homepage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("Hi there this is useEffect, imma dispatch fetchrandom");
+    dispatch(fetchRandom());
+  }, []);
+
   return (
     <div>
       <h2>I'm bored</h2>
