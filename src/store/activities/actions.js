aa64a2c6e6e1e9f33c2bf6f418753f253ff6f188
Itsapon <robintmier@gmail.com>
2021-12-03 13:44:03 +0100

    added logic for fetching in action dispatch. Next is setting up a dispatch action in the activity page to test it

--- src/store/activities/actions.js
+++ src/store/activities/actions.js
@@ -11,6 +11,7 @@ const loadStore = (activity) => {
 };
 
 //THUNK
+//fetches one random activity without arguments
 export const fetchRandom = () => {
   return async (dispatch, getState) => {
     console.log("this is thunk speaking");
@@ -23,3 +24,57 @@ export const fetchRandom = () => {
     }
   };
 };
+
+//fetches one random activity either with one arguments or two. 
+
+export const fetchSpecific =(activityType, activityPeople)=>{
+  /*needs two arguments, one for type, one for ppl. Type is string, people is int. 
+  if statement to look for how many arguments there are. 
+  Two different requests: one with one spot in the req, and one with two spots. 
+  If there's only one argument, goes to first req. etc
+
+  
+  */
+ const type=activityType; 
+ const people=activityPeople;
+ if (type==="select"||people===-1){
+   if(type==="select"){
+    return async (dispatch, getState)=> {
+      console.log("this is specific thunk with one arg");
+      try{
+        const res = await axios.get(`${ApiUrl}?participants=${people}`)
+        console.log("this is res for people: ", res.data)  
+      }catch (e){
+        console.log(e)
+      }
+   }}
+   if (people===-1){
+    return async (dispatch, getState)=> {
+      console.log("this is specific thunk with one arg");
+      try{
+        const res = await axios.get(`${ApiUrl}?type=${type}`)
+        console.log("this is res for type: ", res.data)   
+       
+      }catch (e){
+        console.log(e)
+      }}
+    }else{
+     console.log("something went wrong in drop down menus");
+     return
+   }
+  }
+if (type!=="select"&&people!==-1){
+  return async (dispatch, getState)=> {
+    console.log("this is specific thunk with two args");
+    try{
+      const res = await axios.get(`${ApiUrl}?type=${type}&participants=${people}`)
+      console.log("res of fetch with two args: ", res)
+      //just do fetch request with the defined consts at the start of this function.
+
+    }catch (e){
+      console.log(e)
+    }
+  }
+
+}
+}
